14a4954057f51ffefb03abeb9f3b82d3c8e60436
Jirayu Kaewprateep <jkaewprateep@gmail.com>
2024-03-24 21:47:25 -0400

    Capstone project update

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -28,9 +28,11 @@ app.use(express.json());
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 //{{insert code here}}
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 
 const pinoHttp = require('pino-http');
