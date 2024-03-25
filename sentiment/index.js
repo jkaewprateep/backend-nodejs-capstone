113f2804dc25d3d8149987aef0369f7ac0b0d0eb
Jirayu Kaewprateep <jkaewprateep@gmail.com>
2024-03-24 23:11:34 -0400

    update sentiment function

--- sentiment/index.js
+++ sentiment/index.js
@@ -1,28 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+// const logger = require('./logger');
+// const logger = require('./node_modules/logger');
 const logger = require('./logger');
-const expressPino = require('express-pino-logger')({ logger });
+
+// const logger = require('./node_modules/logger');
+// const expressPino = require('express-pino-logger')({ logger });
+// const pinoLogger = require('./logger');
+
 // Task 1: import the natural library
-const natural = {{insert code here}}
+const natural = require("natural");
 
 // Task 2: initialize the express server
-{{insert code here}}
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(expressPino);
+// app.use(expressPino);
 
 // Define the sentiment analysis route
 // Task 3: create the POST /sentiment analysis
-app.{{insert method here}}('{{insert route here}}', async (req, res) => {
+
+app.post("/sentiment", async (req, res) => {
 
     // Task 4: extract the sentence parameter
-    const { sentence } = {{insert code here}};
+    const { sentence } = req.query;
+
+    console.log(sentence)
 
 
     if (!sentence) {
         logger.error('No sentence provided');
+        // log.console('No sentence provided');
         return res.status(400).json({ error: 'No sentence provided' });
     }
 
@@ -38,20 +48,41 @@ app.{{insert method here}}('{{insert route here}}', async (req, res) => {
         let sentiment = "neutral";
 
         // Task 5: set sentiment to negative or positive based on score rules
-        {{insert code here}}
+        // {{insert code here}}
+        if ( analysisResult < 0 )
+        {
+            sentiment = "negative";
+        }
+        else if ( (analysisResult > 0) && (analysisResult < 0.33) )
+        {
+            sentiment = "neutral";
+        } 
+        else
+        {
+            sentiment = "positive";
+        }
 
         // Logging the result
         logger.info(`Sentiment analysis result: ${analysisResult}`);
+        // log.console(`Sentiment analysis result: ${analysisResult}`);
 
         // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
-        {{insert code here}}
+        // {{insert code here}}
+        res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
+
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
+        // log.console(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
-        {{insert code here}}
+        // {{insert code here}}
+        res.status(500).json({ "error": error.info });
     }
 });
 
 app.listen(port, () => {
-    logger.info(`Server running on port ${port}`);
+    // logger.error(`Server running on port ${port}`);
+    logger.info('/ called');
+
+    // log.console(`Server running on port ${port}`);
+    // pinoLogger.info(`Server running on port ${port}`);
 });
